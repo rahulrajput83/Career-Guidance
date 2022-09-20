50dd59263f1960150172d75622a2100b1b62ef8a
rahulrajput83 <rahulrajput21112@gmail.com>
2022-09-20 20:20:07 +0530

    Fixed error

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const AskQuestion = require('./Router/AskQuestion')
 const mongoose = require('mongoose');
 const Answers = require('./Router/Answer');
-const CareerDetail = require('./Router/CareerDetail')
+/* const CareerDetail = require('./Router/CareerDetail') */
 
 const app = express();
 
@@ -38,7 +38,7 @@ app.use(function (req, res, next) {
 
 app.use('/', AskQuestion);
 app.use('/', Answers);
-app.use('/', CareerDetail);
+/* app.use('/', CareerDetail); */
 
 app.get('/', function(req, res) {
     res.send('You are at Home')
